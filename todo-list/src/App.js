c408372c490ca4564d80ccffba5156ac2c9c8713
Izabela <104584812+izabela-jelovac@users.noreply.github.com>
2023-04-29 23:24:10 +0200

    assign value to the input

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,17 +1,31 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [newItem, setNewItem] = useState("");
   return (
     <>
       <form className="new-item-form">
         <div className="form-row">
           <label htmlFor="item">New Item</label>
-          <input type="text" id="item" />
+          <input
+            value={newItem}
+            onChange={(e) => setNewItem(e.target.value)}
+            type="text"
+            id="item"
+          />
         </div>
         <button className="btn">Add</button>
       </form>
       <h1 className="header">Todo List</h1>
       <ul className="list">
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 1
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
         <li>
           <label>
             <input type="checkbox" />
